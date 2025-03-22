9469b3d13f8e1dd59b7cc557d5c4bad7d3d75fd1
d3fenddr <ggeasyggeasy79@gmail.com>
2025-03-22 19:39:33 +0400

    Initial create

---
+++ RestaurantAPI/RestaurantAPI/Models/Reservation.cs
@@ -0,0 +1,12 @@
+﻿namespace RestaurantAPI.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public DateTime Date { get; set; }
+        public int Guests { get; set; }
+        public string SpecialRequest { get; set; }
+    }
+}
---
+++ RestaurantAPI/RestaurantAPI/Models/Restaurant.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantAPI.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string WorkingHours { get; set; }
+    }
+}
