b2d6c6a6779c76dd1de8b65c386655dc74aff93f
d3fenddr <ggeasyggeasy79@gmail.com>
2025-04-09 00:40:13 +0400

    small css changes

--- restaurant-client/src/App.tsx
+++ restaurant-client/src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
 import AdminDashboard from './pages/AdminDashboard';
